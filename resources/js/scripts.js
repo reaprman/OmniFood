7d7a28995034ef8b98c72fa1277cf1a6c98b898e
ReaprMan <ryan.logan01@gmail.com>
2018-05-25 01:31:16 -0500

    scrolling animations added to menu

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -7,9 +7,35 @@ $(document).ready(function() {
             $('nav').addClass('sticky'); // replaces sticky tag in html file: class="sticky"
         } else {
             $('nav').removeClass('sticky');
-            }
+        }
     }, {
         offset: '60px;' // menu will now appear 60px before the next section
     });
 
     /* Scroll on buttons */
+    $('.js--scroll-to-plans').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
+
+    $('.js--scroll-to-start').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+
+    /* Navigation scroll */
+
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+            || location.hostname == this.hostname) {
+    
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+               if (target.length) {
+                 $('html,body').animate({
+                     scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+    

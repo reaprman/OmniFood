a462784daba72a8caf4e3b2c571ed6fae11dff8c
ReaprMan <ryan.logan01@gmail.com>
2018-05-24 23:39:39 -0500

    corrections to js file

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,13 +1,9 @@
 $(document).ready(function() {
-    $(document).ready(function() {
-
-        $('.js--section-features').waypoint(function(direction) {
-             if (direction == "down") {
-                $('nav').addClass('stick'); // replaces sticky tag in html file: class="sticky"
-             } else {
-                $('nav').removeClass('sticky');
-             }
-        })
-        
+    $('.js-section-features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('stick'); // replaces sticky tag in html file: class="sticky"
+        } else {
+            $('nav').removeClass('sticky');
+            }
+        })   
     });
-});

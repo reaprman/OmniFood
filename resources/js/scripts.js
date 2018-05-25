9f4349abfe212386757462adfe935a3f45ff53de
ReaprMan <ryan.logan01@gmail.com>
2018-05-25 00:58:44 -0500

    js and html modifications

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,19 +1,15 @@
 
-/* for the sticky navigation */
 $(document).ready(function() {
+
+    /* for the sticky navigation */
     $('.js--section-features').waypoint(function(direction) {
         if (direction == "down") {
             $('nav').addClass('sticky'); // replaces sticky tag in html file: class="sticky"
         } else {
             $('nav').removeClass('sticky');
             }
-        })   
     }, {
         offset: '60px;' // menu will now appear 60px before the next section
     });
 
     /* Scroll on buttons */
-
-    $('js--scroll-to-plans').click(function() {
-        $('html, body').animate({scrollTop: $('js--section-plans').offset().top}, 1000)
-    })

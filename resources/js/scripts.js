90c19377b250b68a678096b28640afa8fcf269b5
ReaprMan <ryan.logan01@gmail.com>
2018-05-25 00:44:49 -0500

    added scrolling animation using js. edits of class names in html file

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,9 +1,19 @@
+
+/* for the sticky navigation */
 $(document).ready(function() {
     $('.js--section-features').waypoint(function(direction) {
         if (direction == "down") {
-            $('nav').addClass('stick'); // replaces sticky tag in html file: class="sticky"
+            $('nav').addClass('sticky'); // replaces sticky tag in html file: class="sticky"
         } else {
             $('nav').removeClass('sticky');
             }
         })   
+    }, {
+        offset: '60px;' // menu will now appear 60px before the next section
     });
+
+    /* Scroll on buttons */
+
+    $('js--scroll-to-plans').click(function() {
+        $('html, body').animate({scrollTop: $('js--section-plans').offset().top}, 1000)
+    })

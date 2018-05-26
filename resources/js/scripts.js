7ee1565dff724dc85fc3214c9c3c1d360efb64d0
ReaprMan <ryan.logan01@gmail.com>
2018-05-26 09:19:57 -0500

    completion of scrolling animation changes in html, css, js

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
     }, {
         offset: '60px;' // menu will now appear 60px before the next section
     });
-
+    
     /* Scroll on buttons */
     $('.js--scroll-to-plans').click(function() {
         $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
@@ -23,25 +23,45 @@ $(document).ready(function() {
 
     /* Navigation scroll */
 
-    $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-            || location.hostname == this.hostname) {
-    
+    $(function() {
+        $('a[href*="#"]:not([href="#"])').click(function() {
+          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-               if (target.length) {
-                 $('html,body').animate({
-                     scrollTop: target.offset().top
-                }, 1000);
-                return false;
+            if (target.length) {
+              $('html, body').animate({
+                scrollTop: target.offset().top
+              }, 1000);
+              return false;
             }
-        }
+          }
+        });
+      });
 
-        /* Animations on scroll */
+      /* Animations on scroll */
 
-        $('.js--wp-1').waypoint(function(direction){
-            $('js--wp-1').addClass('animated fadeIn');
-        })
+      $('.js--wp-1').waypoint(function(direction) {
+          $('.js--wp-1').addClass('animated fadeIn');
+      }, {
+          offset: '50%'
+      });
+
+      $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
     });
 });
     
